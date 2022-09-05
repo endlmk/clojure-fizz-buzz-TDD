624a8525baec080cc90ea3a96a7f330b0902dea6
endlmk <1269467+endlmk@users.noreply.github.com>
2022-09-06 00:06:40 +0900

    Done FizzBuzz.

--- src/fizz_buzz/core.clj
+++ src/fizz_buzz/core.clj
@@ -1,13 +1,19 @@
 (ns fizz-buzz.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
-
 (defn convert [n]
   (cond
+    (and (= (rem n 3) 0) (= (rem n 5) 0)) "Fizzbuzz"
     (= (rem n 3) 0) "Fizz"
+    (= (rem n 5) 0) "Buzz"
     :else (str n)))
   
+(defn fizzbuzzlist [n]
+  (map convert (range 1 (+ n 1))))
+
+(defn -main
+  "fizzbuzz 100"
+  [& args]
+  (doseq [item (fizzbuzzlist 100)] 
+    (println item)))
+    
--- test/fizz_buzz/core_test.clj
+++ test/fizz_buzz/core_test.clj
@@ -7,10 +7,12 @@
 ;;  - [x] 2を渡すと文字列2を返す
 ;; - [x] 3の倍数のときは「Fizz」に変換する
 ;;  - [x] 3を渡すと「Fizz」を返す
-;; - [ ] 5の倍数のときは「Buzz」に変換する
-;; - [ ] 3と5両方の倍数のときは「FizzBuzz」に変換する
+;; - [x] 5の倍数のときは「Buzz」に変換する
+;;  - [x] 5を渡すと「Buzz」を返す
+;; - [x] 3と5両方の倍数のときは「FizzBuzz」に変換する
+;;  - [x] 15を渡すと「FizzBuzz」を返す
 
-(deftest fizzbuzz-test-number
+(deftest fizzbuzz-test-convert
   (testing "数を文字列に変換する"
     (testing "1を渡すと文字列1を返す"
       (is (= "1" (convert 1))))
@@ -20,5 +22,24 @@
   (testing "3の倍数のときは「Fizz」に変換する"
     (testing "3を渡すと「Fizz」を返す"
       (is (= "Fizz" (convert 3))))
+    )
+  (testing "5の倍数のときは「Buzz」に変換する"
+    (testing "5を渡すと「Buzz」を返す"
+      (is (= "Buzz" (convert 5))))
+    )
+  (testing "3と5両方の倍数のときは「FizzBuzz」に変換する"
+    (testing "15を渡すと「FizzBuzz」を返す"
+      (is (= "Fizzbuzz" (convert 15))))
+    )
+  )
+
+;; - [x] 1〜nまでのFizzBuzzの列を返す
+;;  - [x] 1〜15までの列を返す
+
+(deftest fizzbuzz-test-list
+  (testing "1〜nまでのFizzBuzzの列を返す"
+    (testing "1〜15までの列を返す"
+      (is (= ["1" "2" "Fizz" "4" "Buzz" "Fizz" "7" "8" "Fizz" "Buzz" "11" "Fizz" "13" "14" "Fizzbuzz"]
+             (fizzbuzzlist 15))))
     )
   )
